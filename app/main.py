33c53e68ac532d0c9e4fb92abe35ee22418cf65a
Ermolenko Andrey <f1nd6r@gmail.com>
2025-04-21 15:00:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,48 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __str__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def take_damage(self, damage: int) -> None:
+        if self.health > 0:
+            self.health -= damage
+            print(
+                f"{self.name} reserve {damage} damage! "
+                f"Health {self.health} now."
+            )
+            if self.health <= 0:
+                self.die()
+
+    def die(self) -> None:
+        print(f"{self.name} die.")
+        Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health -= 50
+            if herbivore.health <= 0:
+                herbivore.die()
